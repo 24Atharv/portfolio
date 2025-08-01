b4951c2a6e5cd38bc287da5acf8ea019bde674b4
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-01 13:21:46 +0530

    nodejs, expressjs icon add

--- src/TechstackIcon/ReactIcon.tsx
+++ src/TechstackIcon/ReactIcon.tsx
@@ -1,4 +1,4 @@
-export const ReactIconComponent = () => {
+export const ReactIcon = () => {
     return <div className="flex justify-center items-center gap-2 p-2 bg-gray-700 rounded">
         <img width="24" height="24" src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/24/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png" alt="external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo"/>
         <span>React</span>
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,11 +1,16 @@
-import { JsComponent } from "../TechstackIcon/JsComponent"
-import { ReactIconComponent } from "../TechstackIcon/ReactComponent"
-import { TailwindComponent } from "../TechstackIcon/tailwindComponent"
+import { ExpressIcon } from "../TechstackIcon/ExpressIcon"
+import { JsIcon } from "../TechstackIcon/JsIcon"
+import { NodejsIcon } from "../TechstackIcon/NodejsIcon"
+import { ReactIcon } from "../TechstackIcon/ReactIcon"
+import { TailwindIcon } from "../TechstackIcon/TailwindIcon"
 
 export const TechStack = () => {
     return <div className="border border-gray-200 p-4 rounded-lg flex gap-4">
-        <JsComponent />
-        <TailwindComponent />
-        <ReactIconComponent />
+        <JsIcon />
+        <TailwindIcon />
+        <ReactIcon />
+        <NodejsIcon />
+        <ExpressIcon />
     </div>
 }
+
