c71456d97e90237795bf92f164f526bba22f3f65
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-01 11:23:22 +0530

    react button

---
+++ src/TechstackIcon/ReactComponent.tsx
@@ -0,0 +1,6 @@
+export const ReactIconComponent = () => {
+    return <div className="flex justify-center items-center gap-2 p-2 bg-gray-700 rounded">
+        <img width="24" height="24" src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/24/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png" alt="external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo"/>
+        <span>React</span>
+    </div>
+}
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,9 +1,11 @@
 import { JsComponent } from "../TechstackIcon/JsComponent"
+import { ReactIconComponent } from "../TechstackIcon/ReactComponent"
 import { TailwindComponent } from "../TechstackIcon/tailwindComponent"
 
 export const TechStack = () => {
     return <div className="border border-gray-200 p-4 rounded-lg flex gap-4">
         <JsComponent />
         <TailwindComponent />
+        <ReactIconComponent />
     </div>
 }
