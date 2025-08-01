086a1cd10bc92be41a06173b14d70b2b55967087
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-01 17:17:32 +0530

    padding issue

--- src/TechstackIcon/ReactIcon.tsx
+++ src/TechstackIcon/ReactIcon.tsx
@@ -1,5 +1,5 @@
 export const ReactIcon = () => {
-    return <div className="flex justify-center items-center gap-2 px-6 bg-gray-700 rounded">
+    return <div className="flex justify-center items-center gap-2 px-6 py-2 bg-gray-700 rounded">
         <img width="24" height="24" src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/24/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png" alt="external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo"/>
         <span className="font-semibold text-sm">React</span>
     </div>
