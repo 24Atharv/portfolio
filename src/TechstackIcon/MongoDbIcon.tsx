8c1dbf5fc6e49221b4ad570d99f1905ea37ab70a
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-01 13:33:04 +0530

    ts, mongo Icon add

---
+++ src/TechstackIcon/MongoDbIcon.tsx
@@ -0,0 +1,9 @@
+import mongoIcon from '../assets/icon.png'
+
+export const MongoDbIcon = () => {
+    return <div className="flex justify-center items-center gap-2 p-2 bg-gray-700 rounded">
+        {/* <img src={mongoIcon} height="24px" width="24px" alt="" /> */}
+        <img width="24" height="24" src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/external-mongodb-a-cross-platform-document-oriented-database-program-logo-shadow-tal-revivo.png" alt="external-mongodb-a-cross-platform-document-oriented-database-program-logo-shadow-tal-revivo"/>
+    <span>MongoDB</span>
+    </div>
+}
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,8 +1,10 @@
 import { ExpressIcon } from "../TechstackIcon/ExpressIcon"
 import { JsIcon } from "../TechstackIcon/JsIcon"
+import { MongoDbIcon } from "../TechstackIcon/MongoDbIcon"
 import { NodejsIcon } from "../TechstackIcon/NodejsIcon"
 import { ReactIcon } from "../TechstackIcon/ReactIcon"
 import { TailwindIcon } from "../TechstackIcon/TailwindIcon"
+import { TypeScriptIcon } from "../TechstackIcon/TypeScriptIcon"
 
 export const TechStack = () => {
     return <div className="border border-gray-200 p-4 rounded-lg flex gap-4">
@@ -11,6 +13,8 @@ export const TechStack = () => {
         <ReactIcon />
         <NodejsIcon />
         <ExpressIcon />
+        <TypeScriptIcon />
+        <MongoDbIcon />
     </div>
 }
 
