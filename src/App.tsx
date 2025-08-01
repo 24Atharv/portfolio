c4783b5e071ddc52e814a9a56b2bf7c5d64f0f08
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-01 11:06:24 +0530

    jsComponent added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
-import { Intro } from './components/intro'
+import { Intro } from './components/Intro'
 import { Navbar } from './components/Navbar'
+import { TechStack } from './components/TechStack'
 
 function App() {
   return <div className='bg-black min-h-screen text-white'>
     <div className='p-10 flex flex-col justify-center items-center'>
       <Navbar />
       <Intro />
+      <TechStack />
     </div>
   </div>
 }
---
+++ src/components/TechStack.tsx
@@ -0,0 +1,7 @@
+import { JsComponent } from "../TechstackIcon/JsComponent"
+
+export const TechStack = () => {
+    return <div className="border border-gray-200 p-4 rounded-lg">
+        <JsComponent />
+    </div>
+}
