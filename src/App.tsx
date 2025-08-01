49eb29a05c31a4088b572922f3ca17774ed51a5e
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-01 09:15:45 +0530

    navbar comp add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { Intro } from './components/intro'
+import { Navbar } from './components/Navbar'
 
 function App() {
   return <div className='bg-black min-h-screen text-white'>
-    <div className='p-10 flex justify-center'>
-    <Intro />
+    <div className='p-10 flex flex-col justify-center items-center'>
+      <Navbar />
+      <Intro />
     </div>
   </div>
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import { HomeIcon } from "../icons/HomeIcon"
+
+export const Navbar = () => {
+    return <div className="border border-gray-200 rounded-xl bg-white text-black p-3 flex">
+        <HomeIcon />
+    </div>
+}
