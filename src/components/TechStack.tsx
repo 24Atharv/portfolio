943851e2aea6aca81b528abf879fd262325f79a0
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-01 11:16:08 +0530

    tailwindComponent Added

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,7 +1,9 @@
 import { JsComponent } from "../TechstackIcon/JsComponent"
+import { TailwindComponent } from "../TechstackIcon/tailwindComponent"
 
 export const TechStack = () => {
-    return <div className="border border-gray-200 p-4 rounded-lg">
+    return <div className="border border-gray-200 p-4 rounded-lg flex gap-4">
         <JsComponent />
+        <TailwindComponent />
     </div>
 }
