6f1bac3b147d71885cf279911836da683101aa52
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-01 09:26:23 +0530

    ProjectIcon added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import { HomeIcon } from "../icons/HomeIcon"
+import { ProjectIcon } from "../icons/projectIcon"
 
 export const Navbar = () => {
-    return <div className="border border-gray-200 rounded-xl bg-white text-black p-3 flex">
+    return <div className="border border-gray-200 rounded-xl bg-[#71717a] text-white
+    px-20 py-3 flex gap-4">
         <HomeIcon />
+        <ProjectIcon />
     </div>
 }
